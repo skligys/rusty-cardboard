aebe815ac602c89538035b26a47f718d6c33d1e2
Skirmantas Kligys <Skirmantas.Kligys@gmail.com>
2014-07-22 16:35:16 -0700

    Make main.c minimal.  Couldn't make it work with android_main in Rust.

--- jni/main.c
+++ jni/main.c
@@ -1,102 +1,19 @@
-/*
- * Copyright (C) 2010 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-//BEGIN_INCLUDE(all)
-#include <jni.h>
-#include <errno.h>
-
-#include <EGL/egl.h>
-#include <GLES2/gl2.h>
-
-#include <android/sensor.h>
-#include <android/log.h>
 #include <android_native_app_glue.h>
 
-#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
-#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
-
-/**
- * Our saved state data.
- */
-struct saved_state {
-    float angle;
-    int32_t x;
-    int32_t y;
-};
-
-/**
- * Shared state for our app.
- */
-struct engine {
-    struct android_app* app;
-
-    ASensorManager* sensorManager;
-    const ASensor* accelerometerSensor;
-    ASensorEventQueue* sensorEventQueue;
-
-    int animating;
-    EGLDisplay display;
-    EGLSurface surface;
-    EGLContext context;
-    int32_t width;
-    int32_t height;
-    struct saved_state state;
-};
-
 /* Functions implemented in Rust. */
-extern int init_display(struct engine* engine);
-extern void draw_frame(struct engine* engine);
-extern void term_display(struct engine* engine);
-extern int32_t handle_input(struct android_app* app, AInputEvent* event);
-extern void handle_cmd(struct android_app* app, int32_t cmd);
 extern void rust_android_main(struct android_app* app);
-extern void rust_event_loop(struct android_app* app, struct engine* engine);
 
 /**
- * This is the main entry point of a native application that is using
- * android_native_app_glue.  It runs in its own thread, with its own
- * event loop for receiving input events and doing other things.
+ * This is the main entry point of a native application that is using android_native_app_glue.
+ * It runs in its own thread, with its own event loop for receiving input events and doing other
+ * things.
+ *
+ * TODO: Figure out why skipping this and implementing anroid_main in Rust leads to much smaller
+ * libnative-activity.so, which cannot be loaded from Java.
  */
 void android_main(struct android_app* app) {
-    struct engine engine;
-
-    LOGI("-------------------------------------------------------------------");
-
     // Make sure glue isn't stripped.
     app_dummy();
 
-    memset(&engine, 0, sizeof(engine));
-    app->userData = &engine;
-    app->onAppCmd = handle_cmd;
-    app->onInputEvent = handle_input;
-    engine.app = app;
-
-    // Prepare to monitor accelerometer
-    engine.sensorManager = ASensorManager_getInstance();
-    engine.accelerometerSensor = ASensorManager_getDefaultSensor(engine.sensorManager,
-            ASENSOR_TYPE_ACCELEROMETER);
-    engine.sensorEventQueue = ASensorManager_createEventQueue(engine.sensorManager,
-            app->looper, LOOPER_ID_USER, NULL, NULL);
-
-    if (app->savedState != NULL) {
-        // We are starting with a previous saved state; restore from it.
-        engine.state = *(struct saved_state*)app->savedState;
-    }
-
-    rust_event_loop(app, &engine);
+    rust_android_main(app);
 }
-//END_INCLUDE(all)
